6c27d9db5af8f1a5e2c133cb0b9ae3e193b8a0f4
sequws <workspaceshare@gmail.com>
2022-02-08 21:36:08 +0100

    Add example app for testing chart and indicator controls

---
+++ ChartsAndIndicators/Sample/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ControlzEx.Theming;
+using Prism.Ioc;
+using Prism.Modularity;
+using Sample.Views;
+using System.Threading;
+using System.Windows;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            var lang = Sample.Properties.Settings.Default.AppLanguage;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lang);
+
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterForNavigation<DemoView1>("Demo1View"); register in MainWindow with region
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the application theme to Dark.Green
+            //ThemeManager.Current.ChangeTheme(this, "Dark.Green");
+        }
+    }
+}
---
+++ ChartsAndIndicators/Sample/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MahApps.Metro.Controls;
+using Prism.Regions;
+using System.Windows;
+
+namespace Sample.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow(IRegionManager regionManager)
+        {
+            InitializeComponent();
+
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(DemoView1));
+        }
+    }
+}
