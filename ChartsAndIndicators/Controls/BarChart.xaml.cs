360a2b68c1e9a42b82a5db2741eeabcdf766b248
sequws <workspaceshare@gmail.com>
2022-02-11 23:38:59 +0100

    Add Canvas to BarChart control, INotifyPropertyChange implementation

--- ChartsAndIndicators/Controls/BarChart.xaml.cs
+++ ChartsAndIndicators/Controls/BarChart.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,15 +20,64 @@ namespace Controls
     /// <summary>
     /// Interaction logic for BarChart.xaml
     /// </summary>
-    public partial class BarChart : UserControl
+    public partial class BarChart : UserControl, INotifyPropertyChanged
     {
-        public string Name { get; set; } = "test";
+        private string _chartName;
+        public string ChartName
+        {
+            get => _chartName;
+            set => SetField(ref _chartName, value);
+        }
 
         public BarChart()
         {
-            InitializeComponent();
+            InitializeComponent();           
 
             DataContext = this;
+            ChartName = "Test Name";
+            
+        }
+
+        private void DrawMainRect(int margin = 10)
+        {
+            MainCanvas.Children.Clear();
+            Rectangle rect = new Rectangle();
+            Canvas.SetLeft(rect, margin);
+            Canvas.SetTop(rect, margin);
+            rect.Height = MainCanvas.ActualHeight - 2 * margin;
+            rect.Width = MainCanvas.ActualWidth - 2 * margin;
+            rect.Stroke = Brushes.Gray;
+            rect.StrokeThickness = 2;
+
+            MainCanvas.Children.Add(rect);
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawMainRect();
         }
     }
 }
