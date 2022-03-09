dc7840926d4550cb25d86b9d69abd7de07d21cab
sequws <workspaceshare@gmail.com>
2022-03-09 21:47:20 +0100

    Update drawing lines method

--- ChartsAndIndicators/Controls/LineChartDrawer.cs
+++ ChartsAndIndicators/Controls/LineChartDrawer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -44,20 +45,35 @@ namespace Controls
 
         public void DrawLines(Dictionary<string, List<double>> linesData)
         {
-            Line line = new Line();
-            line.X1 = 10;
-            line.X2 = 100;
-            line.Y1 = 10 * ScaleCalculator.Scale;
-            line.Y2 = 50 * ScaleCalculator.Scale;
-            line.Stroke = Brushes.OrangeRed;
-            line.StrokeThickness = 2;
+            //Line line = new Line();
+            //line.X1 = 10;
+            //line.X2 = 100;
+            //line.Y1 = 10 * ScaleCalculator.Scale;
+            //line.Y2 = 50 * ScaleCalculator.Scale;
+            //line.Stroke = Brushes.OrangeRed;
+            //line.StrokeThickness = 2;
+            //canvas.Children.Add(line);
 
-            canvas.Children.Add(line);
+            Brush[] brushes = { Brushes.Red, Brushes.Green, Brushes.Blue, Brushes.Orange, Brushes.DarkGray };
 
-
+            int brushNum = 0;
             foreach (var lineData in linesData)
             {
-                
+                PointCollection points = new PointCollection();
+                int i = 0;
+                foreach(var data in lineData.Value)
+                {
+                    points.Add(new Point(i * 40, ScaleCalculator.LineZeroY - data * ScaleCalculator.Scale));
+                    i++;
+                }
+
+                Polyline polyline = new Polyline();
+                polyline.StrokeThickness = 1;
+                polyline.Stroke = brushes[brushNum % brushes.Count()];
+                polyline.Points = points;
+
+                canvas.Children.Add(polyline);
+                brushNum++;
             }
 
         }
