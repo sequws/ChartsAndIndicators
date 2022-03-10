d77b733246730b4aef428b3aa51dc70c1ad6cbb0
sequws <workspaceshare@gmail.com>
2022-03-10 21:10:15 +0100

    Update LineChartDemo, add button to generate random lines

--- ChartsAndIndicators/Sample/Views/LineChartDemo.xaml.cs
+++ ChartsAndIndicators/Sample/Views/LineChartDemo.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace Sample.Views
 {
@@ -7,9 +9,23 @@ namespace Sample.Views
     /// </summary>
     public partial class LineChartDemo : UserControl
     {
+        Dictionary<string, List<double>> linesData = new Dictionary<string, List<double>>();
+
         public LineChartDemo()
         {
             InitializeComponent();
+        }
+
+        private void RandomLinesButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            //MainLineChart.Reset();
+
+            int lines = rnd.Next(5, 10);
+            for (int i = 0; i < lines; i++)
+            {
+                //MainLineChart.AddBar(BarNameTextBox.Text + i, rnd.Next(-200, 200));
+            }
         }
     }
 }
