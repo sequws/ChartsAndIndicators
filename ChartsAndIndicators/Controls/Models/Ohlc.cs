d9ef28b6ee16e90a60171f38f6d752324ce7e069
sequws <workspaceshare@gmail.com>
2022-03-23 20:28:13 +0100

    Add Ohlc model as data for OhlcChart

---
+++ ChartsAndIndicators/Controls/Models/Ohlc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls.Models
+{
+    public  class Ohlc
+    {
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public double Volume { get; set; }
+
+        public Ohlc(double open, double high, double low, double close)
+        {
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+        }
+    }
+}
--- ChartsAndIndicators/Sample/Views/OhlcChart.xaml.cs
+++ ChartsAndIndicators/Sample/Views/OhlcChart.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using Controls.Models;
+using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace Sample.Views
 {
@@ -7,9 +9,13 @@ namespace Sample.Views
     /// </summary>
     public partial class OhlcChartDemo : UserControl
     {
+        List<Ohlc> ohlcCandles = new List<Ohlc>();
+
         public OhlcChartDemo()
         {
             InitializeComponent();
+
+
         }
     }
 }
