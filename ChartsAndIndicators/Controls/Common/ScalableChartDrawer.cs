853a683abe4d94ba9a6a75331e3c7d7472b03564
sequws <workspaceshare@gmail.com>
2022-03-27 13:59:15 +0200

    Add new ChartDrawer type for scalable charts

---
+++ ChartsAndIndicators/Controls/Common/ScalableChartDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Controls.Common
+{
+    /// <summary>
+    /// Allow draw chart which center can be moved and scaled
+    /// </summary>
+    public class ScalableChartDrawer
+    {
+        public double CenterX { get; set; }
+        public double CenterY { get; set; }
+        public double ViewHeight { get; set; }
+        public double ViewWidth { get; set; }
+
+        Canvas canvas;
+    }
+}
