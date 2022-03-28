66d37ccc422379fb960b9f245c095ab73b2b5253
sequws <workspaceshare@gmail.com>
2022-03-28 22:18:04 +0200

    Add calculator for ScalableChartDrawer

---
+++ ChartsAndIndicators/Controls/Common/ScalableScaleCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Controls.Common
+{
+
+    public class ScalableScaleCalculator
+    {
+        public double Scale { get; set; }
+        public Point Center { get; set; }
+
+        // without canvas reference
+        private double ctrlWidth = 0;
+        private double ctrlHeight = 0;
+
+        // visible area on chart
+        private double viewHeight = 0;
+        private double viewWidth = 0;
+
+        // highest and lowest point from data
+        private double dataHigh;
+        private double dataLow;
+        private double dataLength;
+
+    }
+}
