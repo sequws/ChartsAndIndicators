7cd613cd40c0d8cebb9212ad1ad9f65b28243198
sequws <workspaceshare@gmail.com>
2022-05-06 18:46:59 +0200

    Add method drawing arc segment

--- ChartsAndIndicators/Controls/PieChartDrawer.cs
+++ ChartsAndIndicators/Controls/PieChartDrawer.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Controls
 {
@@ -12,8 +15,8 @@ namespace Controls
     {
         public double ChartMargin { get; set; } = 20;
 
-
         Canvas canvas;
+
 
         public PieChartDrawer(Canvas canvas)
         {
@@ -23,8 +26,47 @@ namespace Controls
         public void Draw(List<PiePart> data)
         {
             if (data.Count == 0) return;
+            canvas.Children.Clear();
 
             var sum = data.Sum(x => x.Value);
+            CreateArcSegment(canvas);
+
+        }
+
+        private void CreateArcSegment(Canvas canvas)
+        {
+            var controlWidth = canvas.ActualWidth;
+            var controlHeight = canvas.ActualHeight;
+            var centerPoint = new Point(controlWidth / 2, controlHeight / 2);
+
+            PathFigure pthFigure = new PathFigure();
+            pthFigure.StartPoint = new Point(centerPoint.X -50, centerPoint.Y); ;
+
+            ArcSegment arcSeg = new ArcSegment();
+            arcSeg.Point = new Point(centerPoint.X + 50 , centerPoint.Y);
+            arcSeg.Size = new Size(50, 50);
+            arcSeg.IsLargeArc = true;
+            arcSeg.SweepDirection = SweepDirection.Counterclockwise;
+            arcSeg.RotationAngle = 0;
+
+            PathSegmentCollection myPathSegmentCollection = new PathSegmentCollection();
+            myPathSegmentCollection.Add(arcSeg);
+
+            pthFigure.Segments = myPathSegmentCollection;
+
+            PathFigureCollection pthFigureCollection = new PathFigureCollection();
+            pthFigureCollection.Add(pthFigure);
+
+            PathGeometry pthGeometry = new PathGeometry();
+            pthGeometry.Figures = pthFigureCollection;
+
+            Path arcPath = new Path();
+            arcPath.Stroke = new SolidColorBrush(Colors.Black);
+            arcPath.StrokeThickness = 1;
+            arcPath.Data = pthGeometry;
+            arcPath.Fill = new SolidColorBrush(Colors.Yellow);
+
+            canvas.Children.Add(arcPath);
         }
     }
 }
